680c6ba0814ec690bb347f18067a6bc761573197
TivaR8 <tiva.rait@ocsbstudent.ca>
2018-04-06 15:08:18 -0400

    Update

--- FactorialDoWhileTivaR/FactorialWhileForm.Designer.cs
+++ FactorialDoWhileTivaR/FactorialWhileForm.Designer.cs
@@ -28,20 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.lstFactorial = new System.Windows.Forms.ListBox();
+            this.btnGetAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(180, 196);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(64, 13);
+            this.lblAnswer.TabIndex = 0;
+            this.lblAnswer.Text = "The Answer";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(156, 52);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber.TabIndex = 1;
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(101, 36);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(96, 13);
+            this.lblInstruction.TabIndex = 2;
+            this.lblInstruction.Text = "Enter your number:";
+            // 
+            // lstFactorial
+            // 
+            this.lstFactorial.FormattingEnabled = true;
+            this.lstFactorial.Location = new System.Drawing.Point(156, 78);
+            this.lstFactorial.Name = "lstFactorial";
+            this.lstFactorial.Size = new System.Drawing.Size(120, 95);
+            this.lstFactorial.TabIndex = 3;
+            // 
+            // btnGetAnswer
+            // 
+            this.btnGetAnswer.Location = new System.Drawing.Point(175, 220);
+            this.btnGetAnswer.Name = "btnGetAnswer";
+            this.btnGetAnswer.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAnswer.TabIndex = 4;
+            this.btnGetAnswer.Text = "Get Answer";
+            this.btnGetAnswer.UseVisualStyleBackColor = true;
+            this.btnGetAnswer.Click += new System.EventHandler(this.btnGetAnswer_Click);
             // 
             // frmFactorialWhile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(455, 275);
+            this.Controls.Add(this.btnGetAnswer);
+            this.Controls.Add(this.lstFactorial);
+            this.Controls.Add(this.lblInstruction);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblAnswer);
             this.Name = "frmFactorialWhile";
             this.Text = "Factorial Do While By Tiva Rait";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.ListBox lstFactorial;
+        private System.Windows.Forms.Button btnGetAnswer;
     }
 }
 
--- FactorialDoWhileTivaR/FactorialWhileForm.cs
+++ FactorialDoWhileTivaR/FactorialWhileForm.cs
@@ -7,6 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+/*
+ * Created by: Tiva Rait
+ * Created on: 6-04-2018
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #19 - Factorial Do While
+ * This program calculates the factroial of any number the user enters
+*/
 
 namespace FactorialDoWhileTivaR
 {
@@ -15,6 +22,49 @@ namespace FactorialDoWhileTivaR
         public frmFactorialWhile()
         {
             InitializeComponent();
+
+            // Hide the answer text
+            lblAnswer.Hide();
+        }
+
+        private void btnGetAnswer_Click(object sender, EventArgs e)
+        {
+            // Declare the variables
+            Double factorialAnswer;
+            Double factorialNumber;
+            int factorialCounter;
+
+            // Clear the list box
+            this.lstFactorial.Items.Clear();
+
+            // Set final answer to 1
+            factorialNumber = 1;
+
+            // Set counter to 0
+            factorialCounter = 0;
+
+            // Get the users number from thw text box
+            factorialAnswer = Convert.ToDouble(this.txtNumber.Text);
+
+            // Start to do the calculations of the factorials
+            do
+            {
+                // Increment the counter
+                factorialCounter = factorialCounter + 1;
+
+                // Show the counter number in the list box for the user
+                this.lstFactorial.Items.Add(factorialCounter);
+
+                // Get previous number 
+                factorialNumber =  
+                // Multiply the factorial counter by answer
+                factorialAnswer = factorialCounter * factorialAnswer;
+
+            } while (factorialCounter <+ factorialAnswer);
+
+            // Display the answer
+            lblAnswer.Show();
+            this.lblAnswer.Text = this.txtNumber.Text + "! =" + Convert.ToString(factorialAnswer);
         }
     }
 }
